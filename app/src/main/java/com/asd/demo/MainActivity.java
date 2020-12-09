be64c256a927aee440009877ba9666a9eb41375c
shipingxue <799651927@qq.com>
2020-12-09 10:22:37 +0800

    fix

--- app/src/main/java/com/asd/demo/MainActivity.java
+++ app/src/main/java/com/asd/demo/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        //
         if (id == R.id.action_settings) {
             return true;
         }
